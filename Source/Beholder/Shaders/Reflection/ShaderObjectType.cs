a02d106e2558c3d1ea8a5f9b90fc6201b4d867d3
Daniil Rodin <furidan@gmail.com>
2014-01-26 17:22:20 +0600

    Adjusted license text

--- Source/Beholder/Shaders/Reflection/ShaderObjectType.cs
+++ Source/Beholder/Shaders/Reflection/ShaderObjectType.cs
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2010-2013 Beholder Project - Daniil Rodin
+Copyright (c) 2010-2014 Beholder Project - Daniil Rodin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
